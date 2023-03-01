aee29deef9773db1ac63fee867a4feb96ca957aa
jungmal <jungmal@yonsei.ac.kr>
2023-03-01 19:40:07 +0900

    [design] 닉네임 추천 화면 구현

---
+++ feature-login/src/main/java/com/ddd/pollpoll/feature/login/ui/NicknameViewModel.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ddd.pollpoll.feature.login.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ddd.pollpoll.core.result.Result
+import com.ddd.pollpoll.core.result.asResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NicknameViewModel @Inject constructor(
+) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<NicknameUiState> = MutableStateFlow(NicknameUiState.Empty)
+    val uiState = _uiState.asStateFlow()
+
+    fun addNickname(token: String) {
+
+    }
+}
+
+sealed interface NicknameUiState {
+
+    object Empty : NicknameUiState
+    data class Error(val throwable: Throwable) : NicknameUiState
+    data class Success(val data: String) : NicknameUiState
+    object Loading : NicknameUiState
+}
