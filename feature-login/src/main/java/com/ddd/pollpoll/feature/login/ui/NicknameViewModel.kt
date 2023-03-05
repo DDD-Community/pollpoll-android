67c2f8974bfa0c3e3122542047081fb88710bca9
vov1252@naver.com <vov1252@naver.com>
2023-03-05 21:27:25 +0900

    [feature] nickname viewmodel 완성

--- feature-login/src/main/java/com/ddd/pollpoll/feature/login/ui/NicknameViewModel.kt
+++ feature-login/src/main/java/com/ddd/pollpoll/feature/login/ui/NicknameViewModel.kt
@@ -18,6 +18,8 @@ package com.ddd.pollpoll.feature.login.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ddd.pollpoll.Nickname
+import com.ddd.pollpoll.core.data.NickNameRepository
 import com.ddd.pollpoll.core.result.Result
 import com.ddd.pollpoll.core.result.asResult
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -29,20 +31,58 @@ import javax.inject.Inject
 
 @HiltViewModel
 class NicknameViewModel @Inject constructor(
+    private val nickNameRepository: NickNameRepository
 ) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<NicknameUiState> = MutableStateFlow(NicknameUiState.Empty)
-    val uiState = _uiState.asStateFlow()
+    private val _recommendNicknameUiState: MutableStateFlow<RecommendNicknameUiState> =
+        MutableStateFlow(RecommendNicknameUiState.Empty)
+    val recommendNicknameUiState = _recommendNicknameUiState.asStateFlow()
 
-    fun addNickname(token: String) {
+    private val _insertNicknameUiState: MutableStateFlow<InsertNicknameUiState> =
+        MutableStateFlow(InsertNicknameUiState.Empty)
+    val insertNicknameUiState = _insertNicknameUiState.asStateFlow()
+
+
+    fun recommendNickname() = viewModelScope.launch {
+        nickNameRepository.getNickNameRecommend().asResult().collect {
+            _recommendNicknameUiState.value = when (it) {
+                is Result.Success -> RecommendNicknameUiState.Success(it.data?.nickname ?: "")
+                is Result.Error -> RecommendNicknameUiState.Error(
+                    it.exception ?: Exception("알수없는 오류")
+                )
+                Result.Loading -> RecommendNicknameUiState.Loading
+            }
+
+        }
+
+        fun insertNickName(nickName: String) = viewModelScope.launch {
+            nickNameRepository.putNickname(Nickname(nickName)).asResult().collect {
+                _insertNicknameUiState.value = when (it) {
+                    is Result.Success -> InsertNicknameUiState.Success
+                    is Result.Error -> InsertNicknameUiState.Error(
+                        it.exception ?: Exception("알수없는 오류")
+                    )
+                    Result.Loading -> InsertNicknameUiState.Loading
+                }
+            }
+        }
+
 
     }
 }
 
-sealed interface NicknameUiState {
+sealed interface RecommendNicknameUiState {
 
-    object Empty : NicknameUiState
-    data class Error(val throwable: Throwable) : NicknameUiState
-    data class Success(val data: String) : NicknameUiState
-    object Loading : NicknameUiState
+    object Empty : RecommendNicknameUiState
+    data class Error(val throwable: Throwable) : RecommendNicknameUiState
+    data class Success(val data: String) : RecommendNicknameUiState
+    object Loading : RecommendNicknameUiState
+}
+
+sealed interface InsertNicknameUiState {
+
+    object Empty : InsertNicknameUiState
+    data class Error(val throwable: Throwable) : InsertNicknameUiState
+    object Success : InsertNicknameUiState
+    object Loading : InsertNicknameUiState
 }
